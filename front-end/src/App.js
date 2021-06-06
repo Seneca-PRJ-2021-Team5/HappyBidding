da91d62889a13cf20072ee99f595675a458c380c
Arthur Dibe <arthurverissimo82@gmail.com>
2021-06-06 15:58:35 -0400

    added in the Router list HomePage and NotFound

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,17 +7,22 @@ import dashboard from './dashboard'; //import dashboard.js
 import recoveryAccount from './recoveryAccount'; //import dashboard.js 
 import signup from './signup';
 import profile from './profile';
+import HomePage from './HomePage'
+import NotFound from './NotFound'
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 class App extends React.Component {
   render(){
     return(
       <BrowserRouter>
         <Switch>
+          <Route exact path="/" component={HomePage} />　
           <Route exact path="/login" component={login} />　
           <Route exact path="/signup" component={signup}/>
           <Route exact path="/profile" component={profile} />
           <Route exact path="/dashboard" component={dashboard} />　
           <Route exact path="/recoveryAccount" component={recoveryAccount} />　
+          <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
     );
