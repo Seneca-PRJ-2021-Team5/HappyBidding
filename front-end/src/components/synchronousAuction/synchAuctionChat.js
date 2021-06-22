7c47db31ab7f1fda07d41efb2a7378fc4d852675
Adam Gariba <agariba101@gmail.com>
2021-06-22 10:09:19 -0400

    fixed synchAuctionChat to display all messages in array

--- front-end/src/components/synchronousAuction/synchAuctionChat.js
+++ front-end/src/components/synchronousAuction/synchAuctionChat.js
@@ -6,30 +6,47 @@ import Messages from './Messages/messages';
 
 const ENDPOINT = "http://localhost:5000"
 
+let socket;
+
 function SynchAuctionChat(props){ 
 
     const [response, setResponse] = useState("");
+    const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
 
     useEffect(() => {
-        const socket = socketIOClient(ENDPOINT);
-    }, []);
+        socket = socketIOClient(ENDPOINT);
+    }, [ENDPOINT]);
+
+    useEffect(() => {
+        socket.on('message', m => {
+            setMessages([...messages, m.text]);
+        });
+    }, [messages]);
 
     function handleSubmit(event){
         console.log("TEST")
     }
 
+    const sendMessage = (event) => {
+        event.preventDefault();
+    
+        if(message) {
+          socket.emit('sendMessage', message, () => setMessage(""));
+        }
+    }
+
     return(
             <React.Fragment>
-            <div id="chatScreen">
-                <Messages messages={messages}/>
-            </div>
-            <form id="chatForm" class="input-group" onSubmit={handleSubmit}>
-                <input type="text" class="form-control" placeholder="Text to chat" />
-                <div class="input-group-append">
-                    <input class="btn btn-outline-info" type="submit" value="Send" />
+                <div id="chatScreen">
+                    {messages.map((message, i) => <p id={i}>{message}</p>)}
                 </div>
-                    </form>
+                <form id="chatForm" class="input-group" onSubmit={handleSubmit}>
+                    <input type="text" class="form-control" placeholder="Text to chat" value={message} onChange={({ target: { value } }) => setMessage(value)} />
+                    <div class="input-group-append">
+                        <input class="btn btn-outline-info" type="submit" value="Send" onClick={e => sendMessage(e)} />
+                    </div>
+                </form>
             </React.Fragment>
     );
 }
