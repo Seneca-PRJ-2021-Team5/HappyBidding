5edf9db069e1704f5fd64944ea9e6d4df5e1505f
Arthur Dibe <arthurverissimo82@gmail.com>
2021-07-06 05:30:13 -0400

    fixed the camel case for the allowFullScreen in line 32

--- front-end/src/components/synchronousAuction/liveStreaming.js
+++ front-end/src/components/synchronousAuction/liveStreaming.js
@@ -29,7 +29,7 @@ function LiveStreaming(props){
                title="YouTube video player" 
                frameborder="0" 
                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-               allowfullscreen>
+               allowFullScreen>
             </iframe>
         </div>
     );
