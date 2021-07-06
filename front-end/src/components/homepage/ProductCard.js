1ed79290ebf060bb62fa60819c4cdc16d85f0163
Arthur Dibe <arthurverissimo82@gmail.com>
2021-07-06 05:28:47 -0400

    added function to allow user to register to an auction listed in the home page

--- front-end/src/components/homepage/ProductCard.js
+++ front-end/src/components/homepage/ProductCard.js
@@ -1,7 +1,30 @@
 import { Card, Button, Container, Row, Col } from 'react-bootstrap'
+import { withRouter } from 'react-router-dom';
 
 const ProductCard = (props)=>
 {
+
+    const registerUserToAuction =(productID) =>
+    {
+        if(sessionStorage.getItem("emailAddress"))
+        {
+            fetch(`https://happybiddingserve.herokuapp.com/api/user/auction?id=${productID}&emailAddress=${sessionStorage.getItem("emailAddress")}`, {
+                method: "POST",
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(response => response.json()) 
+            .then(json => {
+                console.log(json)
+                props.history.push("/profile")
+            })
+            .catch(err => console.log(err));
+        }
+        else 
+        {
+            props.history.push("/login")
+        }
+    }
+
     return(
         <>
             <Container>
@@ -10,7 +33,7 @@ const ProductCard = (props)=>
                     props.products.map((product, index)=>
                     {
                         return(
-                            <Col key={index}>
+                            <Col key={product._id}>
                                 <Card style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src={product.image} />
                                     <Card.Body>
@@ -18,7 +41,7 @@ const ProductCard = (props)=>
                                         <Card.Text>
                                             {product.description}
                                         </Card.Text>
-                                        <Button variant="primary">CLICK TO BID</Button>
+                                        <Button variant="primary" onClick={()=>{registerUserToAuction(product._id)}}>CLICK TO BID</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
@@ -31,4 +54,4 @@ const ProductCard = (props)=>
     )
 }
 
-export default ProductCard;
+export default withRouter(ProductCard);
