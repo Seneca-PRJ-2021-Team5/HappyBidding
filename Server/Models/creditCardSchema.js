9afffd291c45f595131626957f0f7f8bbf68a0e0
Arthur Dibe <arthurverissimo82@gmail.com>
2021-07-13 00:24:33 -0400

    set default values to creditCard schema

--- Server/Models/creditCardSchema.js
+++ Server/Models/creditCardSchema.js
@@ -2,12 +2,30 @@ const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let creditCardSchema = new Schema({
-    cardType: String,
-    cardNumber: Number,
-    expiryDate: Date,
-    nameOnCard: String,
-    verificationNumber: String,
-    userEmail: String 
+    cardType:  {
+        type: String, 
+        default: 'none'
+    },
+    cardNumber:  {
+        type: Number, 
+        default: 000000000000
+    },
+    expiryDate:  {
+        type: Date, 
+        default: new Date() 
+    },
+    nameOnCard:  {
+        type: String, 
+        default: 'unkown'
+    },
+    verificationNumber:  {
+        type: String, 
+        default: '000'
+    },
+    userEmail:  {
+        type: String, 
+        default: 'unkown@unkown.com'
+    }, 
 });
 
 const creditCardMODEL = mongoose.model('creditCards', creditCardSchema);
