9cdc2c76b4b2d39af8a637b4448da4179d314804
Adam Gariba <agariba101@gmail.com>
2021-08-08 00:18:30 -0400

    create payment integration component that handles payments

---
+++ front-end/src/components/payment/PaymentIntegration.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from "react";
+import ReactDOM, { render } from "react-dom"
+import { withRouter, useHistory } from 'react-router-dom';
+
+
+const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
+
+function PaymentIntegration(props) {
+
+  const history = useHistory()
+  const [amount, setAmount] = useState("")
+
+  useEffect(() => {
+    console.log("The props: " + props.auctionId)
+    fetch(`http://localhost:5000/api/auctions/${props.auctionId}`)
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+      setAmount(data.auction.price)
+    })
+  })
+
+  const createOrder = (data, actions) =>{
+    console.log(amount)
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: amount,
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then((details) => {
+      console.log("OnApprove: " + details);
+      history.push("/profile")
+    });
+  };
+
+  return (
+    <PayPalButton
+      style={{color:'blue'}}
+      createOrder={(data, actions) => createOrder(data, actions)}
+      onApprove={(data, actions) => onApprove(data, actions)}
+    />
+  );
+}
+
+export default withRouter(PaymentIntegration);
