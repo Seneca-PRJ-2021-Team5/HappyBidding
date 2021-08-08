0b218409dc00e8d648a8f8fa31b46540caee8186
Adam Gariba <agariba101@gmail.com>
2021-08-08 00:55:34 -0400

    made product and auction state setable

--- front-end/src/synchAuction.js
+++ front-end/src/synchAuction.js
@@ -15,6 +15,34 @@ import Chat from './components/synchronousAuction/synchAuctionChat';
 
 //function component
 function SynchronousAuction(props){ 
+
+    const [description, setDescription] = useState({
+        product: {
+            name: "",
+            description: ""
+        },
+        title: "",
+        description: ""
+    })
+
+    useEffect(() => {
+        console.log("The props: " + props.auctionId)
+        fetch(`http://localhost:5000/api/auctions/${props.location.state.auctionId}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+          setDescription({...description,
+                product: {
+                    name: data.auction.product.name,
+                    description: data.auction.product.description
+                },
+                title: data.auction.title,
+                description: data.auction.description
+            })
+        })
+    })
+    
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 767px)' })
     const [values, setValues] = useState({
         usertype: "",
@@ -25,6 +53,7 @@ function SynchronousAuction(props){
     });
 
 
+
   //return page body
   //min-width: 768px => for PC
   //{isTabletOrMobile? <ProfileMobile /> : < ProfilePC />}
@@ -33,7 +62,7 @@ function SynchronousAuction(props){
         <Container>
             <Row>
                 <Col md={3} class="test">  
-                    <Description />
+                    <Description descriptionInfo={description}/>
                 </Col>
                 <Col id="liveStreamingContainer" class="test">
                     <Row>
